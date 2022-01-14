bfda1a988fc6fdf70e6b7a54e23c49c7c6b3a47b
Hayden Brown <nedyah.brown@gmail.com>
2022-01-14 11:21:35 +0000

    init

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+export class MenuHandler {
+    constructor() {  }
+
+    mainMenu() {
+        console.log("Dog Top Trumps");
+        console.log();
+        console.log("MAIN MENU");
+        console.log("1. Play");
+        console.log("2. Quit");
+
+        
+    }
+}
