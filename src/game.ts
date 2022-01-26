65e6449236c27e8d8626df8e18f1f1350ca0e4c2
Hayden Brown <nedyah.brown@gmail.com>
2022-01-26 14:16:51 +0000

    start of file input

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,39 @@
-import { menuHandler } from './index';
+import * as fs from "fs";
+import * as readline from 'readline';
+
+import { menuHandler } from "./index";
+import { Settings } from "./utils";
+
+interface Card {
+    name: string;
+    exercise: number;
+    intelligence: number;
+    friendliness: number;
+    drool: number;
+}
 
 class Game {
-    constructor() {  }
+    constructor() {}
 
-    main() {
-        console.log("main game")
+    private settings: Settings;
+    private cards: Card[];
+
+    async main() {
+        this.settings = await menuHandler.setupMenu();
+        this.loadCards();
+    }
+
+    private loadCards() {
+        let allCards: string[] = [];
+        
+        let lineReader = readline.createInterface({
+            input: fs.createReadStream("dogs.txt")
+        })
+        lineReader.on("line", (line) => {
+            allCards.push(line);
+        })
+
+        console.log(allCards);
     }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
 import * as input from 'input';
 
+export interface Settings {
+    cards: number;
+}
+
 export class MenuHandler {
     constructor() {  }
 
@@ -13,10 +17,22 @@ export class MenuHandler {
 
         let options: object[] = [{name: "Play", value: 0}, {name: "Quit", value: 1}];
         let result = await input.select("Choose an option: ", options);
+
+        console.clear();
         return result;
     }
 
-    async setupMenu(): number {
+    async setupMenu(): Promise<Settings> {
+        console.clear();
+        console.log(this.titleGen("setup menu"))
 
+        let result: number = await input.text("Enter the number of cards to be played (>= 4 and <= 30 and even)");
+        while (result < 4 || result > 30 || result % 2 !== 0) {
+            console.log("Invalid number.")
+            result = await input.text("Enter the number of cards to be played (>= 4 and <= 30 and even)")
+        }
+
+        console.clear();
+        return { cards: result };
     }
 }
